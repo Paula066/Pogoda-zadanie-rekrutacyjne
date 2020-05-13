62f6cee16cc27507353a6eb52c817ce52bb29e8d
Paula066 <36409050+Paula066@users.noreply.github.com>
2020-05-13 22:09:55 +0200

    create new method [chart rain]

--- app.js
+++ app.js
@@ -9,13 +9,49 @@ class App {
     getWeatherData = (data) => {
         this.weatherData = data;
         this.render();
+        this.owlCarousel();
+        this.displaysDescWind();
 
     }
+
+    owlCarousel = () => {
+        $(document).ready(function () {
+            $('.owl-carousel').owlCarousel({
+                items: 16,
+                nav: true,
+                navText: ["<img src='myprevimage.png'>", "<img src='mynextimage.png'>"]
+
+            });
+        });
+    }
+
+    getHigestRain = () => {
+        let higest = 0;
+        this.weatherData.forEach(day => {
+            higest = Math.max(higest, parseFloat(day.rainMm));
+        });
+        return higest;
+    }
+
+    displaysDescWind = () => {
+        let first = 0
+        this.weatherData.forEach(wind => {
+            first = wind.windSpeedkmh;
+        })
+
+        return first;
+    }
+
     render = () => {
-        console.log(this.weatherData)
+        const higestRainMM = this.getHigestRain();
+        const pxPerMM = 10 / higestRainMM;
+        const DescRain = this.displaysDescWind();
+        console.log(this.displaysDescWind())
         this.app = document.querySelector('.whether__box-right');
-        this.app.innerHTML = `<div class="whether__dataHour">${this.weatherData.map(hourData => {
-            return `<div class="whether__boxData">
+        this.app.innerHTML = `
+        <div class="whether__dataHour owl-carousel owl-theme">      
+            ${this.weatherData.map(hourData => {
+            return `<div class="whether__boxData ">
                         <div class="whether__desc whether__desc--hour "></div>
                         <div class="whether__desc whether__desc--hour ">${hourData.hour}:00</div>
                         <div class="whether__desc whether__desc--ico ">
@@ -23,14 +59,18 @@ class App {
                         </div>
                         <div class="whether__desc whether__desc--temperature ">${hourData.temperature}
                         </div>
-                        <div class="whether__desc whether__desc--rain ">${hourData.rainMm}mm</div>
-                        <div class="whether__desc whether__desc--windDegree ">${hourData.windDegree}</div>
-                        <div class="whether__desc whether__desc--windSpeed">${hourData.windSpeed}
+                        <div class="whether__desc whether__desc--rain ">${hourData.rainMm}mm
+                        <div class="whether__background" style="height: ${pxPerMM * parseFloat(hourData.rainMm)}px"></div>
+                        </div>
+                        <div class="whether__desc whether__desc--windDegree ">${hourData.windDegree}
+                            <img class="whether__windImg" src="right.png" alt="">
+                        </div>
+                        <div class="whether__desc whether__desc--windSpeed">${hourData.DescRain}
                             <div class="whether__desc--windSpeedkmh">${hourData.windSpeedkmh} km/h</div>
                         </div>
-                        <div class="whether__desc whether__hpa">${hourData.hpa} hPa</div>
+                        <div class="whether__desc whether__desc--hpa">${hourData.hpa} hPa</div>
                     </div>   
-        
+                    
             `
         }).join('')} 
         `
